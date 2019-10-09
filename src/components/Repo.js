a60d1a4a9a371136aefc4c7b7110a1a102396823
asemqaffaf <asem@qaffaf.com>
2019-10-09 15:17:38 +0300

    first push

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 export default class Repo extends Component {
+
   render() {
+    const {data} = this.props
     return (
       <div style={{ border: '3px red solid' }}>
-        <h6>Repo</h6>
+  
+          <p>{data.title}</p>
+       
+        {/* <h6>{data.title}</h6> */}
       </div>
     );
   }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-
+import Repo from "./Repo"
 export default class Table extends Component {
   render() {
+   const {array} = this.props
     return (
       <div style={{ border: '3px green dotted' }}>
         <h6>Table</h6>
+        <table border="1px solid black">
+          <tr>
+          <th>head</th>
+          <th>head2</th>
+          <th>head3</th>
+          </tr>
+     {array.map((item) => {
+     return(  
+      <tr key={item.id} > 
+        <td>
+      <Repo key={item.id}  data={item} >item.title</Repo> 
+      </td>
+      </tr>
+      )
+     })}
+     </table>
       </div>
     );
   }
